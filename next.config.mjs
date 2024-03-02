a535c92d6a33aa67833231fdabfe81d2044f4b39
Shakti045 <shaktimandal045@gmail.com>
2024-03-02 15:36:55 +0530

    Added

--- next.config.mjs
+++ next.config.mjs
@@ -7,7 +7,10 @@ const nextConfig = {
                 hostname:'api.dicebear.com'
             }
         ]
-    }
+    },
+    eslint: {
+        ignoreDuringBuilds: true,
+    },
 };
 
 export default nextConfig;
